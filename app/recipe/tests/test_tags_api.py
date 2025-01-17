c40fd0e9fc9219c737dd07c4ff87b8bbd0e7ed03
mohammad <mhamdamn456@gmail.com>
2025-01-17 06:41:23 -0800

    add new commit

--- app/recipe/tests/test_tags_api.py
+++ app/recipe/tests/test_tags_api.py
@@ -13,7 +13,12 @@ from core.models import Tag
 from recipe.serializers import TagSerializer
 
 
-TAGS_URL = "recipe:tag-list"
+TAGS_URL = reverse("recipe:tag-list")
+
+
+def detail_url(tag_id):
+    """create and return tag detail url"""
+    return reverse("recipe:tag-detail", args=[tag_id])
 
 
 def create_user(email="user@example.com", password="testpass123"):
@@ -66,3 +71,26 @@ class PrivateTagsApiTests(TestCase):
         self.assertEqual(len(res.data), 1)
         self.assertEqual(res.data[0]["name"], tags.name)
         self.assertEqual(res.data[0]["id"], tags.id)
+
+    def test_update_tag(self):
+        """Test updating tag."""
+        tag = Tag.objects.create(user=self.user, name="after dinner")
+
+        payload = {"name": "Dessert"}
+        url = detail_url(tag.id)
+        res = self.client.patch(url, payload)
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        tag.refresh_from_db()
+        self.assertEqual(tag.name, payload["name"])
+
+    def test_delete_tag(self):
+        """Test deleting tag."""
+        tag = Tag.objects.create(user=self.user, name="Breakfast")
+
+        url = detail_url(tag.id)
+        res = self.client.delete(url)
+
+        self.assertEqual(res.status_code, status.HTTP_204_NO_CONTENT)
+        tags = Tag.objects.filter(user=self.user)
+        self.assertFalse(tags.exists())
