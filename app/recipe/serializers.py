1468dd9a66422d1e51d3fca74f7bb80046712675
mohammad <mhamdamn456@gmail.com>
2025-01-17 12:35:47 -0800

    add ingredient

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -3,7 +3,16 @@ Serializers for recipe APIs
 """
 from rest_framework import serializers
 
-from core.models import Recipe, Tag
+from core.models import Recipe, Tag, Ingredient
+
+
+class IngredientSerializer(serializers.ModelSerializer):
+    """Serializer for ingredients."""
+
+    class Meta:
+        model = Ingredient
+        fields = ["id", "name"]
+        read_only_fields = ["id"]
 
 
 class TagSerializer(serializers.ModelSerializer):
@@ -18,10 +27,11 @@ class TagSerializer(serializers.ModelSerializer):
 class RecipeSerializer(serializers.ModelSerializer):
     """Serializers for recipes"""
     tags = TagSerializer(many=True, required=False)
+    ingredient = IngredientSerializer(many=True, required=False)
 
     class Meta:
         model = Recipe
-        fields = ["id", "title", "time_minutes", "price", "link", "tags"]
+        fields = ["id", "title", "time_minutes", "price", "link", "tags", "ingredient"]
         read_only_fields = ["id"]
 
     def _get_or_create_tags(self, tags, recipe):
@@ -34,20 +44,36 @@ class RecipeSerializer(serializers.ModelSerializer):
             )
             recipe.tags.add(tag_obj)
 
+    def _get_or_create_ingredients(self, ingredient, recipe):
+        """Handle getting or creating ingredeints needed."""
+        auth_user = self.context["request"].user
+        for ingredients in ingredient:
+            ingredient_obj, create = Ingredient.objects.get_or_create(
+                user=auth_user,
+                **ingredients
+            )
+            recipe.ingredient.add(ingredient_obj)
+
     def create(self, validated_data):
         """create a recipe."""
         tags = validated_data.pop("tags", [])
+        ingredients = validated_data.pop("ingredient", [])
         recipe = Recipe.objects.create(**validated_data)
         self._get_or_create_tags(tags, recipe)
+        self._get_or_create_ingredients(ingredients, recipe)
 
         return recipe
 
     def update(self, instance, validated_data):
         """Update recipe."""
         tags = validated_data.pop("tags", None)
+        ingredient = validated_data.pop("ingredient", None)
         if tags is not None:
             instance.tags.clear()
             self._get_or_create_tags(tags, instance)
+        if ingredient is not None:
+            instance.ingredient.clear()
+            self._get_or_create_ingredients(ingredient, instance)
 
         for attr, value in validated_data.items():
             setattr(instance, attr, value)
